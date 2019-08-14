b7b7fe659b1dc210bbe4752e2b22920cd66f9a42
sdamman <47450404+sdamman@users.noreply.github.com>
2019-08-14 13:32:17 -0500

    set up base classes

---
+++ src/County Plot Manager/County Plot Manager/Classes/ViewModelBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace County_Plot_Manager.Classes
+{
+	public class ViewModelBase : INotifyPropertyChanged
+	{
+
+
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler handler = this.PropertyChanged;
+			if (handler != null)
+			{
+				var e = new PropertyChangedEventArgs(propertyName);
+				handler(this, e);
+			}
+		}
+
+		[Conditional("DEBUG")]
+		[DebuggerStepThrough]
+		public void VerifyPropertyName(string propertyName)
+		{
+			if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+			{
+				string msg = "Invalid property name:  " + propertyName;
+				throw new Exception(msg);
+			}
+		}
+
+
+	}
+}
--- src/County Plot Manager/County Plot Manager/Windows/wndMain.xaml.cs
+++ src/County Plot Manager/County Plot Manager/Windows/wndMain.xaml.cs
@@ -10,19 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace County_Plot_Manager
+namespace County_Plot_Manager.Windows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for wndMain.xaml
+	/// </summary>
+	public partial class wndMain : Window
+	{
+		public wndMain()
+		{
+			InitializeComponent();
+		}
+	}
 }
