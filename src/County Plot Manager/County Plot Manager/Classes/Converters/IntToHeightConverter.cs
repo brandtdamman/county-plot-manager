9ccef6d5b0a31a881f00f59b3734781f98c22984
sdamman <sdamman@dpswebapps.com>
2019-12-30 18:24:32 -0600

    Added SimplePlotGroup UserControl.  Added converter to control height of this UserControl.

---
+++ src/County Plot Manager/County Plot Manager/Classes/Converters/IntToHeightConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PlotManager.UI.Classes.Converters
+{
+  public class IntToHeightConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      int numberOfItems = (int)value;
+
+      return (50 * numberOfItems) + 25;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
