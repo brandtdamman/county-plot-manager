7fd16d10f3e3841332a65e38576d7a8db93f3ef4
sdamman <sdamman@dpswebapps.com>
2020-03-07 20:01:51 -0600

    Update ViewModelMain.cs

--- src/County Plot Manager/County Plot Manager/ViewModels/ViewModelMain.cs
+++ src/County Plot Manager/County Plot Manager/ViewModels/ViewModelMain.cs
@@ -4,12 +4,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PlotManager.UI.ViewModels
 {
 	public class ViewModelMain : ViewModelBase
 	{
 
+		private bool CanDeleteEmail = true;
+		RelayCommand commandDeleteEmail;
+		public ICommand CommandDeleteEmail
+		{
+			get
+			{
+				if (commandDeleteEmail == null)
+				{
+					commandDeleteEmail = new RelayCommand(param => DeleteEmail(),
+					param => CanDeleteEmail);
+				}
+				return commandDeleteEmail;
+			}
+		}
+
+		private bool CanDeletePhone = true;
+		RelayCommand commandDeletePhone;
+		public ICommand CommandDeletePhone
+		{
+			get
+			{
+				if (commandDeletePhone == null)
+				{
+					commandDeletePhone = new RelayCommand(param => DeletePhone(),
+					param => CanDeletePhone);
+				}
+				return commandDeletePhone;
+			}
+		}
+
+
+		private void DeleteEmail()
+		{
+
+		}
+		private void DeletePhone()
+		{
+
+		}
 
 	}
 }
